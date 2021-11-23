abc2db9f3c3d4dd6ad91e1a6ace228eaa35e83fb
SidKarthik <sa268592@gmail.com>
2021-11-24 01:07:23 +0530

    ugh fixes

--- notes/settings.py
+++ notes/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,9 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/frontend/build/static')
-]
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, '/frontend/build/static')
+STATIC_ROOT =  BASE_DIR / 'frontend/build/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'frontend/build/static'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
